7227a2df695e543d0d0aa698cec63396135c5988
guswls6212 <guswls6212@gmail.com>
2024-10-21 22:00:25 +0900

    setting화면에서 변경할 언어 누르면 setting화면에 표시되는 언어도 바뀌게 변경

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,8 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              widget.appLocalizations.hello,
+              // widget.appLocalizations.hello,
+              _appLocalizations.hello,
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 20),
@@ -132,34 +133,57 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   final AppLocalizations appLocalizations;
   final Function(Locale) setLocale;
 
   SettingsScreen({required this.appLocalizations, required this.setLocale});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  late AppLocalizations _appLocalizations;
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    _appLocalizations = AppLocalizations.of(context)!;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(appLocalizations.settings),
+        title: Text(_appLocalizations.settings),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(appLocalizations.currentLanguage(appLocalizations.localeName)),
+            Text(_appLocalizations
+                .currentLanguage(_appLocalizations.localeName)),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () => setLocale(Locale('ko', 'KR')),
+              onPressed: () {
+                widget.setLocale(Locale('ko', 'KR'));
+                setState(() {}); // setState 호출 추가
+              },
               child: Text('한국어'),
             ),
             ElevatedButton(
-              onPressed: () => setLocale(Locale('en', 'US')),
+              onPressed: () {
+                widget.setLocale(Locale('en', 'US'));
+                setState(() {}); // setState 호출 추가
+              },
               child: Text('English'),
             ),
             ElevatedButton(
-              onPressed: () => setLocale(Locale('ja', 'JP')),
+              onPressed: () {
+                widget.setLocale(Locale('ja', 'JP'));
+                setState(() {}); // setState 호출 추가
+              },
               child: Text('日本語'),
             ),
           ],
